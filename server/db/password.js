67daad4a5c8aea033d40b0e765b68c583303592d
injong8461 <71006153+injong8461@users.noreply.github.com>
2021-03-18 17:44:49 +0900

    Delete password.js

--- server/db/password.js
+++
@@ -1,8 +0,0 @@
-const mysqlInfo = {
-    host     : 'planbee-db.cspe61htjdkl.ap-northeast-2.rds.amazonaws.com',
-  user     : 'admin',
-  password : "cert0188",
-  database : 'localhost',
-  port : "3306"
-};
-module.exports = mysqlInfo;
