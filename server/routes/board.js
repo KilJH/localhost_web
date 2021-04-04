757df6e586c118cb356c761378ae7f5a150b08f6
HWANG INJONG <hby8461@naver.com>
2021-04-04 20:45:33 +0900

    board update

---
+++ server/routes/board.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const management = require('../api/board');
+const cors = require('cors');
+
+router.use(cors());
+
+router.get('/list', function (req, res, next) {
+	management.list(req, res);
+});
+
+router.post('/write', function (req, res, next) {
+	management.write(req, res);
+});
+
+router.post('/load', function (req, res, next) {
+	management.load(req, res);
+});
+
+module.exports = router;
