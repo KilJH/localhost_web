9e1419d7465694ad82dde4d00866eab91bcbd69e
Jinhyeok Kil <roadjh0201@gmail.com>
2021-04-06 18:22:44 +0900

    make board detail page

--- client/components/board/comment/CommentItem.tsx
+++ client/components/board/comment/CommentItem.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Reply } from '../../../interfaces';
+import UserPhoto from '../../user/UserPhoto';
 
-interface Props {}
+interface Props {
+	comment: Reply;
+}
+
+const Comment = styled.div`
+	display: flex;
+
+	& .nickname {
+		font-weight: 700;
+		margin-bottom: 0.25rem;
+	}
+`;
 
 const CommentItem = (props: Props) => {
-	return <div></div>;
+	const { comment } = props;
+	return (
+		<Comment>
+			<UserPhoto src={'/img/default.jpg'} width={4} margin='0 1rem 0 0' />
+			<div>
+				<div className='nickname'>
+					닉네임<span>{comment.createTime}</span>
+				</div>
+				<div>{comment.description}</div>
+			</div>
+		</Comment>
+	);
 };
 
 export default CommentItem;
