00cecfa325f82015c3bf6d4f058a66cf9ef7279b
Jinhyeok Kil <roadjh0201@gmail.com>
2021-04-16 21:08:57 +0900

    SearchPlace Component

---
+++ client/components/search/SearchPlace.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import { Place } from '../../interfaces';
+import styled from 'styled-components';
+import Button from '../reuse/Button';
+import { useInput } from '../../hooks/useInput';
+import axios from 'axios';
+import { Search } from '@material-ui/icons';
+
+interface Props {
+	setPlace: React.Dispatch<React.SetStateAction<Place>>;
+}
+
+interface PlaceProps {
+	place: Place;
+	setPlace: React.Dispatch<React.SetStateAction<Place>>;
+}
+
+const ItemContainer = styled.div`
+	padding: 1rem;
+	& h4,
+	p {
+		margin: 0;
+	}
+	&:hover {
+		cursor: pointer;
+		background: #eee;
+	}
+`;
+
+const ListContainer = styled.div`
+	max-height: 16em;
+	overflow-y: auto;
+`;
+
+const StyledForm = styled.form`
+	width: 100%;
+	display: flex;
+	& > input {
+		flex: 1;
+	}
+`;
+
+const PlaceItem = (props: PlaceProps) => {
+	const { place, setPlace } = props;
+
+	return (
+		<ItemContainer
+			onClick={() => {
+				setPlace(place);
+			}}
+		>
+			<h4>{place.name}</h4>
+			<p>{place.formatted_address}</p>
+		</ItemContainer>
+	);
+};
+
+const SearchPlace = (props: Props) => {
+	const input = useInput('');
+	const { setPlace } = props;
+	const [places, setPlaces] = useState([]);
+	const onSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		axios.get(`/api/map/searchPlaces?search=${input.value}`).then((res) => {
+			setPlaces(res.data.places);
+		});
+	};
+	return (
+		<div>
+			<StyledForm onSubmit={onSubmit}>
+				<input {...input} />
+				<Button type='submit' padding='0.25rem 1rem'>
+					<Search />
+				</Button>
+			</StyledForm>
+			<ListContainer>
+				{places.map((place) => (
+					<PlaceItem place={place} setPlace={setPlace} />
+				))}
+			</ListContainer>
+		</div>
+	);
+};
+
+export default SearchPlace;
