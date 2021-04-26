f180f25debc9e2b059683b40f88f062d0b76967e
Lee Chanbin <nyamnyam0325@naver.com>
2021-04-26 20:45:25 +0900

    myhosting-prevois applicant list-rating-fix

--- client/components/host/MyHosting/HostPreviousApplicantItem.tsx
+++ client/components/host/MyHosting/HostPreviousApplicantItem.tsx
@@ -11,7 +11,7 @@ export default function HostPreviousApplicantItem({ applicant }: Props) {
 				<td>{applicant?.date || '2021-04-26'}</td>
 				<td>{applicant?.place.name || '월드메르디앙'}</td>
 				<td>{applicant?.user.name || '황인종'}</td>
-				<td>{applicant?.date || '2021-01-01'}</td>
+				<td>{applicant?.rating || '4.5'}</td>
 			</tr>
 		</React.Fragment>
 	);
