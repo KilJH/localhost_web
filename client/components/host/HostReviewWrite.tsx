5b5c64061dee08a4d1fc7b977f198b4a97c6af59
Lee Chanbin <nyamnyam0325@naver.com>
2021-04-27 18:18:19 +0900

    HostReviewWrite-completed

--- client/components/host/HostReviewWrite.tsx
+++ client/components/host/HostReviewWrite.tsx
@@ -1,31 +1,86 @@
-import React from 'react';
+import { Rating } from '@material-ui/lab';
+import React, { useContext, useState } from 'react';
 import styled from 'styled-components';
 import Button from '../reuse/Button';
-import Rating from '../reuse/Rating';
+import { UserStateContext } from '../../context/user';
+import axios from 'axios';
+import SERVER from '../../utils/url';
+import { useInput } from '../../hooks/useInput';
 
 interface Props {
-	id: number;
 	hostUserId: number;
-	description: string;
 }
-
 const TextArea = styled.textarea`
 	width: 100%;
 	resize: none;
 	height: 6em;
 `;
-const RatingStar = styled(Rating)``;
 const SubmitButton = styled(Button)`
 	display: block;
 	margin: 0.25em 0 0 auto;
 `;
+const RatingDiv = styled.div`
+	margin-top: 0.5em;
+	& > label {
+		display: block;
+		margin: 0.25em 0 0 0.25em;
+	}
+`;
+const HostRating = styled(Rating)`
+	&.MuiRating-root {
+		color: #5197d5;
+	}
+`;
 
 const Request = (props: Props) => {
+	const { hostUserId } = props;
+	const [rating, setRating] = useState(0);
+	const [hover, setHover] = useState(-1);
+	const desc = useInput('');
+	const currentUser = useContext(UserStateContext);
+	const onSubmit = async (
+		e: React.MouseEventHandler<HTMLButtonElement, MouseEvent>,
+	) => {
+		e.preventDefault();
+		if (desc.value === '') alert('후기를 작성해주세요!');
+		else if (rating === 0) alert('별점을 등록해주세요!');
+		else {
+			try {
+				const res = await axios.post(`${SERVER}/api/host/reviewWrite`, {
+					id: currentUser.id,
+					hostUserId: hostUserId,
+					description: desc.value,
+					rating: rating,
+				});
+				if (res.data.success) alert('후기가 등록되었습니다!');
+			} catch (err) {
+				return console.log(err);
+			}
+		}
+	};
 	return (
 		<div>
-			<TextArea></TextArea>
-			<RatingStar rating={5.0}></RatingStar>
-			<SubmitButton>리뷰작성</SubmitButton>
+			<TextArea {...desc} placeholder='후기를 작성해주세요'></TextArea>
+			<RatingDiv>
+				<HostRating
+					value={rating}
+					precision={0.5}
+					onChange={(event, newValue) => {
+						setRating(newValue);
+					}}
+					onChangeActive={(event, newHover) => {
+						setHover(newHover);
+					}}
+				/>
+				<label>
+					{hover === -1
+						? rating === 0
+							? '별점을 선택해주세요'
+							: rating
+						: hover}
+				</label>
+			</RatingDiv>
+			<SubmitButton onClick={onSubmit}>리뷰작성</SubmitButton>
 		</div>
 	);
 };
