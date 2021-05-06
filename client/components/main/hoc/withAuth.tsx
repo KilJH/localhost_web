8e2f40a6ddba950e4c001bb7315d7dcfc1c22168
Jinhyeok Kil <roadjh0201@gmail.com>
2021-05-07 02:35:23 +0900

    withAuth HOC with Async

--- client/components/main/hoc/withAuth.tsx
+++ client/components/main/hoc/withAuth.tsx
@@ -3,6 +3,8 @@ import React, { ReactNode } from 'react';
 import SERVER from '../../../utils/url';
 import Router from 'next/router';
 import { User } from '../../../interfaces';
+import { useAsync } from 'react-async';
+import Loading from '../../reuse/Loading';
 
 // isLogined
 // 0: 아무나 볼 수 있음, 1: 로그인한 회원만 볼 수 있음, 2: 게스트만 볼 수 있음
@@ -10,44 +12,65 @@ import { User } from '../../../interfaces';
 // grade
 // 0: 아무나 볼 수 있음, 2: 호스트회원만 볼 수 있음, 3: 관리자회원만 볼 수 있음
 
+const getAuth = async () => {
+	const res = await axios.get(`${SERVER}/api/auth/check`, {
+		withCredentials: true,
+	});
+
+	return res.data;
+};
+
 const withAuth = (isLogined: number, grade: number) => (
-	Component: ReactNode,
+	Component: React.ComponentType<any>,
 ) => {
-	axios.get(`${SERVER}/api/auth/check`, { withCredentials: true }).then(res => {
-		const user: User = res.data.user;
-		if (res.data.success) {
+	return () => {
+		const { data, isLoading, error } = useAsync({
+			promiseFn: getAuth,
+		});
+
+		if (isLoading) return <Loading />;
+		if (error) return <div>에러!</div>;
+		if (!data) return null;
+
+		const user: User = data.user;
+		if (data.success) {
 			console.log('로그인되어있습니다.');
 			if (isLogined === 2) {
 				alert('이미 로그인되어있습니다.');
-				Router.push('/');
+				location.href = '/';
+				return <Loading />;
 			}
 			switch (grade) {
 				// 아무나
 				case 0:
-					return Component;
+					return <Component />;
 				// 호스트
 				case 1:
-					if (user.isHost) return Component;
-					alert('접근할 수 없는 페이지 입니다.');
-					Router.push('/');
+					if (user.isHost) return <Component />;
+					alert('권한이 없습니다. 호스트 신청을 해주세요:D');
+					location.href = '/';
+					break;
 				// 관리자
 				case 2:
-					if (user.isAdmin) return Component;
+					if (user.isAdmin) return <Component />;
 					alert('접근할 수 없는 페이지 입니다.');
-					Router.push('/');
+					location.href = '/';
+					break;
 			}
 		} else {
 			// 로그인되지않음
 			console.log('로그인을 해주세요.');
-			console.log(res.data.message, '이것은 에러');
+
 			if (isLogined === 1) {
-				Router.push('/login');
+				alert('로그인을 해주세요.');
+				location.href = '/login';
 			} else {
-				return Component;
+				return <Component />;
 			}
 		}
-	});
-	return () => <></>;
+
+		return <Loading />;
+	};
 };
 
 export default withAuth;
