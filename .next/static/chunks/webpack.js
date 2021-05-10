ca0aa4bd2ee2cae6e547da7b8fb71b1682b1eb72
Jinhyeok Kil <roadjh0201@gmail.com>
2021-05-10 16:36:01 +0900

    change project structure 2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c4072f0b462e7e3212d7";
+/******/ 	var hotCurrentHash = "56e534ea873210ddfa8d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
