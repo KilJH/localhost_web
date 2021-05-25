5dcdab57333932d46d8c337de82827e989dd893d
Lee Chanbin <nyamnyam0325@naver.com>
2021-05-25 16:51:26 +0900

    admin-mobile nav

--- components/admin/AdminMobileNav.tsx
+++ components/admin/AdminMobileNav.tsx
@@ -5,19 +5,21 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 import Collapse from '@material-ui/core/Collapse';
-import NotificationsOutlinedIcon from '@material-ui/icons/NotificationsOutlined';
-import PeopleOutlinedIcon from '@material-ui/icons/PeopleOutlined';
-import PersonOutlinedIcon from '@material-ui/icons/PersonOutlined';
-import ForumOutlinedIcon from '@material-ui/icons/ForumOutlined';
-import AssignmentIndOutlinedIcon from '@material-ui/icons/AssignmentIndOutlined';
-import AssessmentOutlinedIcon from '@material-ui/icons/AssessmentOutlined';
-import HeadsetMicOutlinedIcon from '@material-ui/icons/HeadsetMicOutlined';
-import AssignmentTurnedInOutlinedIcon from '@material-ui/icons/AssignmentTurnedInOutlined';
 import VpnKeyIcon from '@material-ui/icons/VpnKey';
-import { ArrowDropDownOutlined, ArrowRightOutlined } from '@material-ui/icons';
 import styled from 'styled-components';
 import Router from 'next/router';
-import ArrowLeftOutlined from '@material-ui/icons/ArrowLeftOutlined';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import PeopleIcon from '@material-ui/icons/People';
+import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
+import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
+import AssessmentIcon from '@material-ui/icons/Assessment';
+import HeadsetMicIcon from '@material-ui/icons/HeadsetMic';
+import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
+import { ArrowDropDown, ArrowRight } from '@material-ui/icons';
+import ArrowLeft from '@material-ui/icons/ArrowLeft';
+import BlockIcon from '@material-ui/icons/Block';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 type Props = {
 	selected?: string;
@@ -123,12 +125,16 @@ const ClickedText = styled(ListItemText)`
 export default function AdminLayout(props: Props) {
 	const { selected } = props;
 	const [titleOpen, setTitleOpen] = React.useState(false);
-	const [open, setOpen] = React.useState(true);
+	const [userOpen, setUserOpen] = React.useState(true);
+	const [hostOpen, setHostOpen] = React.useState(true);
+	const handleUserClick = () => {
+		setUserOpen(!userOpen);
+	};
+	const handleHostClick = () => {
+		setHostOpen(!hostOpen);
+	};
 	const titleHandleClick = () => {
 		setTitleOpen(!titleOpen);
-	};
-	const handleClick = () => {
-		setOpen(!open);
 	};
 	return (
 		<NavDiv>
@@ -139,170 +145,215 @@ export default function AdminLayout(props: Props) {
 					</NavTitleIcon>
 					<NavTitleText primary='관리자 페이지' />
 				</NavTitle>
+				<Divider />
 				<Collapse in={titleOpen} timeout='auto' unmountOnExit>
-					<Divider />
-					<List component='div' disablePadding>
-						{/* 공지 관리 */}
-						{selected !== 'notice' ? ( // 미클릭 시
-							<Item button onClick={() => Router.push('/admin/notice/list')}>
-								<Icon>
-									<NotificationsOutlinedIcon />
-								</Icon>
-								<Text primary='공지 관리' />
-							</Item>
-						) : (
-							// 클릭 시
-							<ClickedItem
-								button
-								onClick={() => Router.push('/admin/notice/list')}
-							>
-								<ClickedIcon>
-									<NotificationsOutlinedIcon />
-								</ClickedIcon>
-								<ClickedText primary='공지 관리' />
-							</ClickedItem>
-						)}
-						{/* 게시물 관리 */}
-						{selected !== 'board' ? ( // 미클릭 시
-							<Item button>
-								<Icon>
-									<ForumOutlinedIcon />
-								</Icon>
-								<Text primary='게시물 관리' />
-							</Item>
-						) : (
-							// 클릭 시
-							<ClickedItem button>
-								<ClickedIcon>
-									<ForumOutlinedIcon />
-								</ClickedIcon>
-								<ClickedText primary='게시물 관리' />
-							</ClickedItem>
-						)}
-						{/* 유저 관리 */}
-						<Item button onClick={handleClick}>
+					{/* 공지 관리 */}
+					{selected !== 'notice' ? ( // 미클릭 시
+						<Item button onClick={() => Router.push('/admin/notice')}>
 							<Icon>
-								<PeopleOutlinedIcon />
+								<NotificationsIcon />
 							</Icon>
-							<Text primary='유저 관리' />
-							<ArrowIcon>
-								{open ? <ArrowDropDownOutlined /> : <ArrowLeftOutlined />}
-							</ArrowIcon>
+							<Text primary='공지 관리' />
 						</Item>
-						{/* 일반회원 */}
-						<Collapse in={open} timeout='auto' unmountOnExit>
-							<List component='div' disablePadding>
-								{selected !== 'user' ? ( // 미클릭 시
-									<Item button onClick={() => Router.push('/admin/user/list')}>
-										<ArrowIcon>
-											<ArrowRightOutlined />
-										</ArrowIcon>
-										<Icon>
-											<PersonOutlinedIcon />
-										</Icon>
-										<Text primary='회원' />
-									</Item>
-								) : (
-									// 클릭 시
-									<ClickedItem
-										button
-										onClick={() => Router.push('/admin/user/list')}
-									>
-										<ArrowIcon>
-											<ArrowDropDownOutlined />
-										</ArrowIcon>
-										<ClickedIcon>
-											<PersonOutlinedIcon />
-										</ClickedIcon>
-										<ClickedText primary='회원' />
-									</ClickedItem>
-								)}
-							</List>
-						</Collapse>
-						{/* 호스트 */}
-						<Collapse in={open} timeout='auto' unmountOnExit>
-							<List component='div' disablePadding>
-								{selected !== 'host' ? ( // 미클릭 시
-									<Item button onClick={() => Router.push('/admin/host/list')}>
-										<ArrowIcon>
-											<ArrowRightOutlined />
-										</ArrowIcon>
-										<Icon>
-											<AssignmentIndOutlinedIcon />
-										</Icon>
-										<Text primary='호스트' />
-									</Item>
-								) : (
-									// 클릭 시
-									<ClickedItem
-										button
-										onClick={() => Router.push('/admin/host/list')}
-									>
-										<ArrowIcon>
-											<ArrowDropDownOutlined />
-										</ArrowIcon>
-										<ClickedIcon>
-											<AssignmentIndOutlinedIcon />
-										</ClickedIcon>
-										<ClickedText primary='호스트' />
-									</ClickedItem>
-								)}
-							</List>
-						</Collapse>
-						{/* 호스트 승인 */}
-						{selected !== 'approval' ? ( // 미클릭 시
-							<Item button onClick={() => Router.push('/admin/host/approval')}>
-								<Icon>
-									<AssignmentTurnedInOutlinedIcon />
-								</Icon>
-								<Text primary='호스트 승인' />
-							</Item>
-						) : (
-							// 클릭 시
-							<ClickedItem
-								button
-								onClick={() => Router.push('/admin/host/approval')}
-							>
-								<ClickedIcon>
-									<AssignmentTurnedInOutlinedIcon />
-								</ClickedIcon>
-								<ClickedText primary='호스트 승인' />
-							</ClickedItem>
-						)}
-						{/* 플랜 관리 */}
-						{selected !== 'plan' ? ( // 미클릭 시
-							<Item button>
-								<Icon>
-									<AssessmentOutlinedIcon />
-								</Icon>
-								<Text primary='플랜 관리' />
-							</Item>
-						) : (
-							// 클릭 시
-							<ClickedItem button>
-								<ClickedIcon>
-									<AssessmentOutlinedIcon />
-								</ClickedIcon>
-								<ClickedText primary='플랜 관리' />
-							</ClickedItem>
-						)}
-						{/* 고객센터 관리 */}
-						{selected !== 'customerService' ? (
-							<Item button>
-								<Icon>
-									<HeadsetMicOutlinedIcon />
-								</Icon>
-								<Text primary='고객센터 관리' />
-							</Item>
-						) : (
-							<ClickedItem button>
-								<ClickedIcon>
-									<HeadsetMicOutlinedIcon />
-								</ClickedIcon>
-								<ClickedText primary='고객센터 관리' />
-							</ClickedItem>
-						)}
-					</List>
+					) : (
+						// 클릭 시
+						<ClickedItem button onClick={() => Router.push('/admin/notice')}>
+							<ClickedIcon>
+								<NotificationsIcon />
+							</ClickedIcon>
+							<ClickedText primary='공지 관리' />
+						</ClickedItem>
+					)}
+					{/* 게시물 관리 */}
+					{selected !== 'board' ? ( // 미클릭 시
+						<Item button>
+							<Icon>
+								<ForumIcon />
+							</Icon>
+							<Text primary='게시물 관리' />
+						</Item>
+					) : (
+						// 클릭 시
+						<ClickedItem button>
+							<ClickedIcon>
+								<ForumIcon />
+							</ClickedIcon>
+							<ClickedText primary='게시물 관리' />
+						</ClickedItem>
+					)}
+					{/* 유저 관리 */}
+					<Item button onClick={handleUserClick}>
+						<Icon>
+							<PeopleIcon />
+						</Icon>
+						<Text primary='유저 관리' />
+						<ArrowIcon>
+							{userOpen ? <ArrowDropDown /> : <ArrowLeft />}
+						</ArrowIcon>
+					</Item>
+					{/* 일반회원 */}
+					<Collapse in={userOpen} timeout='auto' unmountOnExit>
+						<List component='div' disablePadding>
+							{selected !== 'user' ? ( // 미클릭 시
+								<Item button onClick={() => Router.push('/admin/user')}>
+									<ArrowIcon>
+										<ArrowRight />
+									</ArrowIcon>
+									<Icon>
+										<PersonIcon />
+									</Icon>
+									<Text primary='회원' />
+								</Item>
+							) : (
+								// 클릭 시
+								<ClickedItem button onClick={() => Router.push('/admin/user')}>
+									<ArrowIcon>
+										<ArrowDropDown />
+									</ArrowIcon>
+									<ClickedIcon>
+										<PersonIcon />
+									</ClickedIcon>
+									<ClickedText primary='회원' />
+								</ClickedItem>
+							)}
+						</List>
+					</Collapse>
+					{/* 차단회원 */}
+					<Collapse in={userOpen} timeout='auto' unmountOnExit>
+						<List component='div' disablePadding>
+							{selected !== 'blacklist' ? ( // 미클릭 시
+								<Item
+									button
+									onClick={() => Router.push('/admin/user/blacklist')}
+								>
+									<ArrowIcon>
+										<ArrowRight />
+									</ArrowIcon>
+									<Icon>
+										<BlockIcon />
+									</Icon>
+									<Text primary='차단회원' />
+								</Item>
+							) : (
+								// 클릭 시
+								<ClickedItem
+									button
+									onClick={() => Router.push('/admin/user/blacklist')}
+								>
+									<ArrowIcon>
+										<ArrowDropDown />
+									</ArrowIcon>
+									<ClickedIcon>
+										<BlockIcon />
+									</ClickedIcon>
+									<ClickedText primary='차단회원' />
+								</ClickedItem>
+							)}
+						</List>
+					</Collapse>
+					{/* 호스트 관리 */}
+					<Item button onClick={handleHostClick}>
+						<Icon>
+							<AccountCircleIcon />
+						</Icon>
+						<Text primary='호스트 관리' />
+						<ArrowIcon>
+							{hostOpen ? <ArrowDropDown /> : <ArrowLeft />}
+						</ArrowIcon>
+					</Item>
+					{/* 호스트 */}
+					<Collapse in={hostOpen} timeout='auto' unmountOnExit>
+						<List component='div' disablePadding>
+							{selected !== 'host' ? ( // 미클릭 시
+								<Item button onClick={() => Router.push('/admin/host')}>
+									<ArrowIcon>
+										<ArrowRight />
+									</ArrowIcon>
+									<Icon>
+										<AssignmentIndIcon />
+									</Icon>
+									<Text primary='호스트' />
+								</Item>
+							) : (
+								// 클릭 시
+								<ClickedItem button onClick={() => Router.push('/admin/host')}>
+									<ArrowIcon>
+										<ArrowDropDown />
+									</ArrowIcon>
+									<ClickedIcon>
+										<AssignmentIndIcon />
+									</ClickedIcon>
+									<ClickedText primary='호스트' />
+								</ClickedItem>
+							)}
+						</List>
+					</Collapse>
+					{/* 호스트 승인 */}
+					<Collapse in={hostOpen} timeout='auto' unmountOnExit>
+						<List component='div' disablePadding>
+							{selected !== 'approval' ? ( // 미클릭 시
+								<Item
+									button
+									onClick={() => Router.push('/admin/host/approval')}
+								>
+									<ArrowIcon>
+										<ArrowRight />
+									</ArrowIcon>
+									<Icon>
+										<AssignmentTurnedInIcon />
+									</Icon>
+									<Text primary='호스트 승인' />
+								</Item>
+							) : (
+								// 클릭 시
+								<ClickedItem
+									button
+									onClick={() => Router.push('/admin/host/approval')}
+								>
+									<ArrowIcon>
+										<ArrowDropDown />
+									</ArrowIcon>
+									<ClickedIcon>
+										<AssignmentTurnedInIcon />
+									</ClickedIcon>
+									<ClickedText primary='호스트 승인' />
+								</ClickedItem>
+							)}
+						</List>
+					</Collapse>
+					{/* 플랜 관리 */}
+					{selected !== 'plan' ? ( // 미클릭 시
+						<Item button onClick={() => Router.push('/admin/plan')}>
+							<Icon>
+								<AssessmentIcon />
+							</Icon>
+							<Text primary='플랜 관리' />
+						</Item>
+					) : (
+						// 클릭 시
+						<ClickedItem button onClick={() => Router.push('/admin/plan')}>
+							<ClickedIcon>
+								<AssessmentIcon />
+							</ClickedIcon>
+							<ClickedText primary='플랜 관리' />
+						</ClickedItem>
+					)}
+					{/* 고객센터 관리 */}
+					{selected !== 'customerService' ? (
+						<Item button>
+							<Icon>
+								<HeadsetMicIcon />
+							</Icon>
+							<Text primary='고객센터 관리' />
+						</Item>
+					) : (
+						<ClickedItem button>
+							<ClickedIcon>
+								<HeadsetMicIcon />
+							</ClickedIcon>
+							<ClickedText primary='고객센터 관리' />
+						</ClickedItem>
+					)}
 				</Collapse>
 			</NavList>
 		</NavDiv>
