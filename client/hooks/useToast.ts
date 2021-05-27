8d81442bfa101fde8f6db5b8a29b7ee52221be9c
Jinhyeok Kil <roadjh0201@gmail.com>
2021-05-27 22:35:12 +0900

    more develop useToast hooks

--- client/hooks/useToast.ts
+++ client/hooks/useToast.ts
@@ -1,10 +1,17 @@
 import { useState } from 'react';
+import { Color } from '@material-ui/lab';
 
 export const useToast = (initialValue: boolean) => {
 	// state 생성 및 초기화
 	const [open, setOpen] = useState(initialValue);
+	const [option, setOption] = useState<{ type: Color; message: string }>({
+		type: 'success',
+		message: '',
+	});
 
-	const handleOpen = () => {
+	const handleOpen = (type?: Color, message?: string) => {
+		if (type || message)
+			setOption({ type: type as Color, message: message as string });
 		setOpen(true);
 	};
 
@@ -15,5 +22,5 @@ export const useToast = (initialValue: boolean) => {
 		setOpen(false);
 	};
 
-	return { open, handleOpen, handleClose };
+	return { open, handleOpen, handleClose, ...option };
 };
