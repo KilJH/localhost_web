f49bb2bfc3c109d1a4df8479cef90f03087549ec
Jinhyeok Kil <roadjh0201@gmail.com>
2021-06-07 16:06:19 +0900

    Greeting component BEM

--- components/Greeting.tsx
+++ components/Greeting.tsx
@@ -1,61 +1,48 @@
-import { Button } from '@material-ui/core';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import Button from './reuse/Button';
 
-const MainImg = styled.img`
-	height: 80vh;
+const Container = styled.div`
+	height: calc(100vh - 10em);
 	width: 100%;
-
-	object-fit: cover;
-
-	display: block;
-	position: absolute;
-	/* top: 0; */
-	left: 0;
-	z-index: -1;
-
-	transition: opacity 1s ease;
-	&.isShow {
-		opacity: 1;
-	}
-	&.isHide {
-		opacity: 0;
-	}
-`;
-
-const Introduction = styled.div`
-	height: 80vh;
-	width: 100%;
-
 	display: flex;
-	/* flex-direction: column; */
 	align-items: center;
 
-	color: white;
-	margin-bottom: 1rem;
-
-	& > div {
-		flex: 1;
-
-		transition: opacity 1s ease;
-		&.isShow {
-			opacity: 1;
+	.greeting {
+		&__image {
+			height: inherit;
+			width: inherit;
+			object-fit: cover;
+			position: absolute;
+			left: 0;
+			z-index: -1;
+			transition: opacity 1s ease;
+			opacity: 0;
+			&--isShow {
+				opacity: 1;
+			}
 		}
-		&.isHide {
+
+		&__text {
+			flex: 1;
+			color: white;
+			transition: opacity 1s ease;
 			opacity: 0;
 			display: none;
+			&--isShow {
+				opacity: 1;
+				display: block;
+			}
+			& + & {
+				position: absolute;
+			}
+			& h1 {
+				font-size: 2.5rem;
+				margin: 0.5rem 0;
+				text-shadow: 0px 0px 16px black;
+			}
 		}
-		& + div {
-			position: absolute;
-		}
-	}
-
-	& > * > h1 {
-		font-size: 2.5rem;
-		font-weight: 900;
-		margin: 0.5rem 0;
-		text-shadow: 0px 0px 16px black;
 	}
 `;
 
@@ -71,36 +58,33 @@ const Greeting = () => {
 		};
 	}, [isShow]);
 	return (
-		<div>
-			<MainImg
-				src='https://source.unsplash.com/collection/43588404/2400x1600'
-				className={isShow ? 'isShow' : 'isHide'}
+		<Container className='greeting'>
+			<img
+				src='https://source.unsplash.com/collection/43588404/2400x1600?1'
+				className={`greeting__image ${isShow ? 'greeting__image--isShow' : ''}`}
 			/>
-			<MainImg
-				src='https://source.unsplash.com/collection/43588404/3000x2000'
-				className={isShow ? 'isHide' : 'isShow'}
+			<img
+				src='https://source.unsplash.com/collection/43588404/3000x2000?2'
+				className={`greeting__image ${isShow ? '' : 'greeting__image--isShow'}`}
 			/>
+			<div
+				className={`greeting__text ${isShow ? 'greeting__text--isShow' : ''}`}
+			>
+				<h1>한 번의 클릭으로 손쉽게 여행을 계획해보세요</h1>
+				<Link href='/plans'>
+					<Button default>베스트 플랜 보기</Button>
+				</Link>
+			</div>
 
-			<Introduction>
-				<div className={isShow ? 'isShow' : 'isHide'}>
-					<h1>한 번의 클릭으로 손쉽게 여행을 계획해보세요</h1>
-					<Link href='/plans'>
-						<Button variant='contained' size='small'>
-							베스트 플랜 보기
-						</Button>
-					</Link>
-				</div>
-
-				<div className={isShow ? 'isHide' : 'isShow'}>
-					<h1>현지인만 아는 로컬맛집을 알고싶다면?</h1>
-					<Link href='/hosts'>
-						<Button variant='contained' size='small'>
-							호스트 보러가기
-						</Button>
-					</Link>
-				</div>
-			</Introduction>
-		</div>
+			<div
+				className={`greeting__text ${isShow ? '' : 'greeting__text--isShow'}`}
+			>
+				<h1>현지인만 아는 로컬맛집을 알고싶다면?</h1>
+				<Link href='/hosts'>
+					<Button default>호스트 보러가기</Button>
+				</Link>
+			</div>
+		</Container>
 	);
 };
 
